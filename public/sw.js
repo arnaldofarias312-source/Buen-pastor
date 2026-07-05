bfa2a3c53f5ccce943703ce17e98ee506dea1cd7
arnaldofarias312-source <arnaldofarias312@gmail.com>
2026-07-05 16:11:18 -0400

    Mejoras PWA: skipWaiting, scope, iOS meta tags

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,18 @@
 const CACHE = "buen-pastor-v1";
 
 self.addEventListener("install", (e) => {
+  self.skipWaiting()
   e.waitUntil(
     caches.open(CACHE).then((c) => c.addAll(["/", "/index.html"]))
-  );
-});
+  )
+})
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(clients.claim())
+})
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request, { ignoreSearch: true }).then((r) => r || fetch(e.request))
-  );
-});
+  )
+})
